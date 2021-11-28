42eefe547a6e76af5d76388775489e970e11dd66
Uttaran <lci2020047@iiitl.ac.in>
2021-11-29 02:24:33 +0530

    Final commit

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.quizapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.quizapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,11 +18,11 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         binding.btnStart.setOnClickListener{
-            if(binding.etName == null || binding.etName?.text.toString().isEmpty()) {
-                Toast.makeText(this, "Please enter a name", Toast.LENGTH_SHORT).show()
-                var b: String? = binding.etName!!.text.toString()
+            if(binding.etName.text.toString().isEmpty()) {
+                Toast.makeText(this@MainActivity, "Please enter a name", Toast.LENGTH_SHORT).show()
             } else {
-                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
                 startActivity(intent)
                 finish()
             }
